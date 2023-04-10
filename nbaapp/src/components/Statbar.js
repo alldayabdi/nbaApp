f50c88b571d5ff7ed7dba5f44a86c4139b315932
Abdikarim Ibrahim <abdikarimibrahim@hotmail.com>
2023-04-10 12:08:07 -0500

    added usestate line of code and query

---
+++ nbaapp/src/components/Statbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {useState} from "react";
+const Statbar = () => {
+    const [query, setQuery] = useState("");
+  return (
+    <div>Statbar</div>
+    
+  )
+}
+
+export default Statbar
