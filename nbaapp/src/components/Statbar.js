7031bc6b276cefb8ebe913a57d861e0c0c994fc6
Abdikarim Ibrahim <abdikarimibrahim@hotmail.com>
2023-04-12 18:20:34 -0500

    Added radio buttons and conditional rendering to differntiate between teams and players

--- nbaapp/src/components/Statbar.js
+++ nbaapp/src/components/Statbar.js
@@ -1,56 +1,76 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+
 const Statbar = () => {
-     // Declare state variables for the search query
+    // Declare state variables for the search query
     const [query, setQuery] = useState("");
     //Declare state variables for the search Results
     const [results, setResults] = useState([]);
+    //Declare state variable for the search type (players or teams)
+    const [searchType, setSearchType] = useState("teams");
 
     useEffect(()=>{
         if (query!==''){
-            //axios request to get team data using search query
-          axios.get(`https://www.balldontlie.io/api/v1/teams?search=${query}&per_page=100`)
-            .then(response =>setResults(response.data.data))
-            .catch(error => console.error(error))
-            
-            //axios request to get player data using search query
-          axios.get(`https://www.balldontlie.io/api/v1/players?search=${query}&per_page=100`)
-            .then(response =>setResults(prevResults => [...prevResults, ...response.data.data]))
-            .catch(error => console.error(error))
+            //axios request to get team or player data using search query and search type
+            axios.get(`https://www.balldontlie.io/api/v1/${searchType}?search=${query}&per_page=100`)
+                .then(response =>setResults(response.data.data))
+                .catch(error => console.error(error))
         }  else {
-          setResults([])
+            setResults([])
         } 
-    
-      }, [query]) // Will only rerun when the query state changes
+    }, [query, searchType]) // Will only rerun when the query or searchType state changes
 
     // Function that handles inputs in the search bar and sets it to the query
-
     function handleInputChange(event) {
         setQuery(event.target.value);
-      }
-  return (
-    <div>
-        <input
-        type = 'text'
-        value = {query}
-        onChange ={handleInputChange}
-        placeholder = "Search stats.."
-        >
-        
-        </input>
-        <button type="submit">Submit</button>
-        <ul>
-  {results.map(result => (
-    <li key={result.id}>{result.abbreviation || result.first_name + ' ' + result.last_name}</li>
-  ))}
+    }
+
+    // Function that handles the selection of search type
+    function handleSearchTypeChange(event) {
+        setSearchType(event.target.value);
+    }
+
+    return (
+        <div>
+            <input
+                type='text'
+                value={query}
+                onChange={handleInputChange}
+                placeholder="Search stats.."
+            />
+
+            <form>
+                <label>
+                    <input 
+                        type="radio" 
+                        value="teams" 
+                        checked={searchType === "teams"} 
+                        onChange={handleSearchTypeChange} 
+                    />
+                    Teams
+                </label>
+                <label>
+                    <input 
+                        type="radio" 
+                        value="players" 
+                        checked={searchType === "players"} 
+                        onChange={handleSearchTypeChange} 
+                    />
+                    Players
+                </label>
+            </form>
+
+            <ul>
+    {results.map(result => (
+        searchType === "players" || result.full_name.toLowerCase().includes(query.toLowerCase()) ? (
+            <li key={result.id}>{result.abbreviation || result.first_name + ' ' + result.last_name}</li>
+        ) : null
+    ))}
 </ul>
 
-
-    </div>
-
-    
-  )
+        </div>
+    )
 }
 
 export default Statbar
